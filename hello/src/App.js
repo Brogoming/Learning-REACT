2a92646c9759f8d35ebcf05ff1bc3f6e1361f5b1
Brogoming <kotag@outlook.com>
2023-05-12 15:00:25 -0500

    Learning about props and useState hooks

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,18 +1,21 @@
 
 import './App.css';
 import Employee from './components/Employee';
+import {useState} from 'react'
 
 function App() {
-  console.log("We are about to list the employees");
+  const [role, setRole] = useState('dev') //const [variableName, setVariableName] = useState(default value)
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ?( //we can do JavaScript logic in HTML if there is { } surrounding the code
         <div>
-          <Employee></Employee> 
-          <Employee></Employee> 
-          <Employee></Employee> 
-          <Employee></Employee> 
+          <input type='text' onChange={(e) => {
+            console.log(e.target.value) //to get what we actually typed in into the input
+            setRole(e.target.value)
+          }}/>
+          <Employee name="Dakota" role="Boss"></Employee> 
+          <Employee name="Walker" role={role}></Employee> 
           <Employee></Employee> 
         </div>
       ) : (
