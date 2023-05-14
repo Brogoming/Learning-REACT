f6e24e1e0ea2927d2c5a87f36282d16c2b0a42f9
Brogoming <kotag@outlook.com>
2023-05-14 13:58:02 -0500

    Playing with Tailwind CSS classes

--- hello/src/App.js
+++ hello/src/App.js
@@ -7,17 +7,25 @@ function App() {
   const [role, setRole] = useState('dev') //const [variableName, setVariableName] = useState(default value)
   const showEmployees = true;
   return (
-    <div className="App bg-red-300">
+    <div class='App'>
       {showEmployees ?( //we can do JavaScript logic in HTML if there is { } surrounding the code
-        <div>
+        <>
           <input type='text' onChange={(e) => {
             console.log(e.target.value) //to get what we actually typed in into the input
             setRole(e.target.value)
           }}/>
-          <Employee name="Dakota" role="Boss"></Employee> 
-          <Employee name="Walker" role={role}></Employee> 
-          <Employee></Employee> 
-        </div>
+          <div class="flex flex-wrap justify-center">
+            <Employee name="Dakota" role="Boss" img="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg"></Employee> 
+            <Employee name="Walker" role={role} img="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg"></Employee> 
+            <Employee img="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg"></Employee> 
+            <Employee name="Dakota" role="Boss" img="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg"></Employee> 
+            <Employee name="Walker" role={role} img="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg"></Employee> 
+            <Employee img="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg"></Employee> 
+            <Employee name="Dakota" role="Boss" img="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg"></Employee> 
+            <Employee name="Walker" role={role} img="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg"></Employee> 
+            <Employee img="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg"></Employee> 
+          </div>
+        </>
       ) : (
         <p>You cannot see the employees.</p>
       )}
