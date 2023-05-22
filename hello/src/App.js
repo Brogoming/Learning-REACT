ba05cdb0e4193ca2b42261ef67006883ae8d61e8
Brogoming <kotag@outlook.com>
2023-05-21 21:42:02 -0500

    made a new navbar with react-route-dom

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
-import AddEmployee from './components/AddEmployee';
-import EditEmployee from './components/EditEmployee';
-import Employee from './components/Employee';
-import {useState} from 'react'
-import {v4 as uuidv4} from 'uuid'
 import Header from './components/Header'
-import Employees from './pages/Employees';
+import Employees from './pages/Employees'
+import Customers from './pages/Customers'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <Header>
-      <Employees />
-    </Header>
+    <BrowserRouter>
+      <Header>
+        <Routes>
+          <Route path='/employees' element={<Employees />} />
+          <Route path='/test' element={<Customers />} />
+        </Routes>
+      </Header>
+    </BrowserRouter>
   );
 }
 
