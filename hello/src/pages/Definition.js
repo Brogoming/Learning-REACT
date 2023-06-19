6ecc4453b38b6051271e20af6d50bf7e2f9c899c
Brogoming <kotag@outlook.com>
2023-06-19 16:25:12 -0500

    Create a Custom Hook (useFetch)

--- hello/src/pages/Definition.js
+++ hello/src/pages/Definition.js
@@ -1,46 +1,20 @@
 import { useState, useEffect } from "react";
-import { useParams, useNavigate, Link } from "react-router-dom";
+import { useParams, useNavigate, Link, useLocation } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import NotFound from "../components/NotFound";
 import DefSearch from "../components/DefSearch";
+import useFetch from "../hooks/UseFetch";
 
 export default function Definition() {
-  const [aWord, setAWord] = useState("");
   let { search } = useParams();
   const navigate = useNavigate();
-  const [notFound, setNotFound] = useState(false);
-  const [error, setError] = useState(false);
+  // const location = useLocation();
 
-  useEffect(() => {
-    const errorUrl = "https://httpstat.us/500";
-    const dictionaryUrl =
-      "https://api.dictionaryapi.dev/api/v2/entries/en/" + search;
-    fetch(dictionaryUrl) //fetches a api
-      .then((response) => {
-        console.log(response.status); //tells us the status of the page
-        if (response.status === 404) {
-          // navigate('/404')
-          setNotFound(true);
-        } else if (response.status === 500) {
-          setError(true);
-        }
+  const [word, errorStatus] = useFetch(
+    "https://api.dictionaryapi.dev/api/v2/entries/en/" + search
+  );
 
-        if (!response.ok) {
-          setError(true);
-          throw new Error("Something went wrong");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setAWord(data[0]);
-        console.log(data[0]);
-      })
-      .catch((e) => {
-        console.log(e.message);
-      });
-  }, []);
-
-  if (notFound === true) {
+  if (errorStatus === 404) {
     return (
       <>
         <NotFound />
@@ -55,7 +29,7 @@ export default function Definition() {
       </>
     );
   }
-  if (error === true) {
+  if (errorStatus) {
     return (
       <>
         <p>Something went wrong!</p>
@@ -70,12 +44,13 @@ export default function Definition() {
       </>
     );
   }
+
   return (
     <>
-      {aWord ? (
+      {word?.[0]?.meanings ? (
         <>
-          <h1>Here is the definition for {aWord.word}</h1>
-          {aWord.meanings.map((meaning) => {
+          <h1>Here is the definition:</h1>
+          {word[0].meanings.map((meaning) => {
             return (
               <p key={uuidv4()}>
                 {meaning.partOfSpeech + ": "}
