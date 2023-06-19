fc93712be18f353529be8da604891af17b720c0a
Brogoming <kotag@outlook.com>
2023-06-19 15:58:35 -0500

    User Register Form and API

---
+++ hello/src/pages/Register.js
@@ -0,0 +1,105 @@
+import { useState, useContext, useEffect } from "react";
+import { baseUrl } from "../shared";
+import { useLocation, useNavigate } from "react-router-dom";
+import { LoginContext } from "../App";
+
+export default function Register() {
+  const [loggedIn, setLoggedIn] = useContext(LoginContext);
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    localStorage.clear();
+    setLoggedIn(false);
+  }, []);
+
+  function login(e) {
+    e.preventDefault(); //doesn't reload the page when we submit
+    const url = baseUrl + "api/register";
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        email: email,
+        password: password,
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        localStorage.setItem("access", data.access);
+        localStorage.setItem("refresh", data.refresh);
+        setLoggedIn(true);
+        navigate(
+          location?.state?.previousUrl
+            ? location.state.previousUrl
+            : "/customers"
+        );
+      });
+  }
+
+  return (
+    <form id="customer" onSubmit={login} className="m-2 w-full max-w-sm">
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/4">
+          <label for="username">Username</label>
+        </div>
+        <div className="md:w-3/4">
+          <input
+            id="username"
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-500"
+            type="text"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/4">
+          <label for="email">Email</label>
+        </div>
+        <div className="md:w-3/4">
+          <input
+            id="email"
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-500"
+            type="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/4">
+          <label id="password">Password</label>
+        </div>
+        <div className="md:w-3/4">
+          <input
+            id="password"
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-500"
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+      <button className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+        Register
+      </button>
+    </form>
+  );
+}
