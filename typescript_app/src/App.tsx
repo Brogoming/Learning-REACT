c3128f4768d7215f6adc04c2c47c1c5b5794cc1f
Brogoming <kotag@outlook.com>
2023-06-26 11:08:28 -0500

    Crypto Price Chart with Chart.js

--- typescript_app/src/App.tsx
+++ typescript_app/src/App.tsx
@@ -3,10 +3,46 @@ import "./App.css";
 import axios from "axios";
 import CryptoSummary from "./components/CryptoSummary";
 import { Crypto } from "./Types";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import type { ChartData, ChartOptions } from "chart.js";
+import moment from "moment";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 function App() {
   const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
   const [selected, setSelect] = useState<Crypto | null>();
+  const [data, setData] = useState<ChartData<"line">>();
+  const [options, setOptions] = useState<ChartOptions<"line">>({
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  });
   //useState is what we use to set up a value
   //for a specific value type for use state we will put <typename> after useState and before ()
   //<typename[] > makes it an array
@@ -30,6 +66,30 @@ function App() {
             //once a option is selected
             const c = cryptos?.find((x) => x.id === e.target.value); //find where x.id === the option selected
             setSelect(c);
+            //request
+            //update data change
+            axios
+              .get(
+                `https://api.coingecko.com/api/v3/coins/${c?.id}/market_chart?vs_currency=usd&days=30&interval=daily`
+              )
+              .then((response) => {
+                console.log(response.data);
+                setData({
+                  labels: response.data.prices.map((price: number[]) => {
+                    return moment.unix(price[0] / 1000).format('MM-DD-YYYY');
+                  }),
+                  datasets: [
+                    {
+                      label: "Price",
+                      data: response.data.prices.map((price: number[]) => {
+                        return price[1];
+                      }),
+                      borderColor: "rgb(255, 99, 132)",
+                      backgroundColor: "rgba(255, 99, 132, 0.5)",
+                    },
+                  ],
+                });
+              });
           }}
           defaultValue="default"
         >
@@ -52,6 +112,11 @@ function App() {
       </div>
       {selected ? <CryptoSummary crypto={selected} /> : null}{" "}
       {/* //if there is a selected element run the Crypto summary function */}
+      {data ? (
+        <div style={{ width: 600 }}>
+          <Line options={options} data={data} />
+        </div>
+      ) : null}
     </>
   );
 }
